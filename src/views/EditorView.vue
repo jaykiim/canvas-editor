3a3c836ec29e0788f15d57a3ef717c4c3828c6e1
jaykiim <tidzmtm12@gmail.com>
2024-03-08 17:52:24 +0900

    feat(): 기본 베이스 UI 구성
    - canvas, gnb, lnb 컴포넌트 UI 생성
    
    Refs:

--- src/views/EditorView.vue
+++ src/views/EditorView.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+import GnbView from './gnb/GnbView.vue';
+import LnbView from './lnb/LnbView.vue';
+import CanvasView from './canvas/CanvasView.vue';
+</script>
+
 <template>
-  <div></div>
+  <GnbView />
+  <div class="editor-container">
+    <LnbView />
+    <CanvasView />
+  </div>
 </template>
+
+<style scoped lang="scss">
+.editor-container {
+  display: flex;
+  width: 100%;
+  height: calc(100% - 3rem);
+}
+</style>
---
+++ src/views/gnb/GnbView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container"></div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  z-index: 1000;
+  background-color: #fff;
+  width: 100%;
+  height: 3rem;
+  border-bottom: 1px solid #e6e6e6;
+}
+</style>
