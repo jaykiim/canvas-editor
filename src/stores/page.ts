d6e15ad39402c7af72b210a533d50a61affc7a92
김재희 <jh.kim4@okestro.com>
2024-03-10 19:36:14 +0900

    feat(page.ts): 테스트용 페이지 객체 2개 추가
    -
    
    Refs:

---
+++ src/stores/page.ts
@@ -0,0 +1,78 @@
+import { ref, computed, reactive } from 'vue'
+import { defineStore } from 'pinia'
+
+export const usePageStore = defineStore('page', () => {
+  const pageStore = reactive<{[id: string]: PageObject}>({
+    '1d5c2a17-ebd8-4075-8c94-8081c668fc20': {
+      id: '1d5c2a17-ebd8-4075-8c94-8081c668fc20',
+      type: 'page-object',
+      name: 'test1',
+      width: 800,
+      height: 600,
+      x: 0,
+      y: 0,
+      scale: 1,
+      parentId: null,
+      children: {
+        '416ce213-d8ac-4b71-9269-48b80729d028': {
+          id: '416ce213-d8ac-4b71-9269-48b80729d028',
+          type: 'design-object',
+          name: 'object1',
+          width: 100,
+          height: 150,
+          x: 100,
+          y: 200,
+          parentId: '1d5c2a17-ebd8-4075-8c94-8081c668fc20',
+          properties: {
+            backgroundColor: '#bbb',
+          },
+          children: {}
+        },
+        'e7a7666e-49a7-4212-9941-4398a7c16a2c': {
+          id: 'e7a7666e-49a7-4212-9941-4398a7c16a2c',
+          type: 'design-object',
+          name: 'object1',
+          width: 100,
+          height: 150,
+          x: 100,
+          y: 200,
+          parentId: '1d5c2a17-ebd8-4075-8c94-8081c668fc20',
+          properties: {
+            lineColor: '#bbb',
+            lineThickness: 2,
+          },
+          children: {}
+        }
+      }
+    },
+    '6d975107-3ffb-422f-8b2d-83a95361738d': {
+      id: '6d975107-3ffb-422f-8b2d-83a95361738d',
+      type: 'page-object',
+      name: 'test2',
+      width: 1200,
+      height: 860,
+      x: 50,
+      y: 120,
+      scale: 1.5,
+      parentId: null,
+      children: {
+        '53be25bf-eb5c-4e36-9364-c0271b921a4b': {
+          id: '53be25bf-eb5c-4e36-9364-c0271b921a4b',
+          type: 'design-object',
+          name: 'testObj1',
+          width: 100,
+          height: 150,
+          x: 600,
+          y: 400,
+          parentId: '6d975107-3ffb-422f-8b2d-83a95361738d',
+          properties: {
+            backgroundColor: '#bbb',
+          },
+          children: {}
+        }
+      }
+    }
+  });
+
+  return { pageStore };
+});
