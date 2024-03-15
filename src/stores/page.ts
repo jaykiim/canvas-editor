66805882ed0489403c377b704f9220a3081cd6c9
jaykiim <tidzmtm12@gmail.com>
2024-03-15 17:06:13 +0900

    feat(pages.ts, Element.ts): 엘리먼트 타입 변경
    - Element의 children 속성 타입 변경
    
    Refs:

--- src/stores/page.ts
+++ src/stores/page.ts
@@ -3,8 +3,8 @@ import { defineStore } from 'pinia'
 import { v4 as uuidv4 } from 'uuid';
 import type { ElementStore, PageElement } from '@/types/Element';
 
-export const useElementStore = defineStore('page', () => {
-  const elementStore = reactive<ElementStore>({
+export const useElementStore = defineStore('element', () => {
+  const store = reactive<ElementStore<PageElement>>({
     list: ['1d5c2a17-ebd8-4075-8c94-8081c668fc20', '6d975107-3ffb-422f-8b2d-83a95361738d'],
     detail: {
       '1d5c2a17-ebd8-4075-8c94-8081c668fc20': {
@@ -18,34 +18,43 @@ export const useElementStore = defineStore('page', () => {
         scale: 1,
         parentId: null,
         children: {
-          '416ce213-d8ac-4b71-9269-48b80729d028': {
-            id: '416ce213-d8ac-4b71-9269-48b80729d028',
-            type: 'shape',
-            name: 'object1',
-            width: 100,
-            height: 150,
-            x: 100,
-            y: 200,
-            parentId: '1d5c2a17-ebd8-4075-8c94-8081c668fc20',
-            properties: {
-              backgroundColor: '#bbb',
+          list: ['416ce213-d8ac-4b71-9269-48b80729d028', 'e7a7666e-49a7-4212-9941-4398a7c16a2c'],
+          detail: {
+            '416ce213-d8ac-4b71-9269-48b80729d028': {
+              id: '416ce213-d8ac-4b71-9269-48b80729d028',
+              type: 'shape',
+              name: 'object1',
+              width: 100,
+              height: 150,
+              x: 100,
+              y: 200,
+              parentId: '1d5c2a17-ebd8-4075-8c94-8081c668fc20',
+              properties: {
+                backgroundColor: '#bbb',
+              },
+              children: {
+                list: [],
+                detail: {}
+              }
             },
-            children: {}
-          },
-          'e7a7666e-49a7-4212-9941-4398a7c16a2c': {
-            id: 'e7a7666e-49a7-4212-9941-4398a7c16a2c',
-            type: 'shape',
-            name: 'object1',
-            width: 100,
-            height: 150,
-            x: 100,
-            y: 200,
-            parentId: '1d5c2a17-ebd8-4075-8c94-8081c668fc20',
-            properties: {
-              lineColor: '#bbb',
-              lineThickness: 2,
-            },
-            children: {}
+            'e7a7666e-49a7-4212-9941-4398a7c16a2c': {
+              id: 'e7a7666e-49a7-4212-9941-4398a7c16a2c',
+              type: 'shape',
+              name: 'object1',
+              width: 100,
+              height: 150,
+              x: 100,
+              y: 200,
+              parentId: '1d5c2a17-ebd8-4075-8c94-8081c668fc20',
+              properties: {
+                lineColor: '#bbb',
+                lineThickness: 2,
+              },
+              children: {
+                list: [],
+                detail: {}
+              }
+            }
           }
         }
       },
@@ -60,19 +69,25 @@ export const useElementStore = defineStore('page', () => {
         scale: 1.5,
         parentId: null,
         children: {
-          '53be25bf-eb5c-4e36-9364-c0271b921a4b': {
-            id: '53be25bf-eb5c-4e36-9364-c0271b921a4b',
-            type: 'shape',
-            name: 'testObj1',
-            width: 100,
-            height: 150,
-            x: 600,
-            y: 400,
-            parentId: '6d975107-3ffb-422f-8b2d-83a95361738d',
-            properties: {
-              backgroundColor: '#bbb',
-            },
-            children: {}
+          list: ['53be25bf-eb5c-4e36-9364-c0271b921a4b'],
+          detail: {
+            '53be25bf-eb5c-4e36-9364-c0271b921a4b': {
+              id: '53be25bf-eb5c-4e36-9364-c0271b921a4b',
+              type: 'shape',
+              name: 'testObj1',
+              width: 100,
+              height: 150,
+              x: 600,
+              y: 400,
+              parentId: '6d975107-3ffb-422f-8b2d-83a95361738d',
+              properties: {
+                backgroundColor: '#bbb',
+              },
+              children: {
+                list: [],
+                detail: {}
+              }
+            }
           }
         }
       }
@@ -91,11 +106,24 @@ export const useElementStore = defineStore('page', () => {
       x: 0,
       y: 0,
       scale: 1,
-      children: {}
+      children: { list: [], detail: {} }
     };
 
-    elementStore.list.push(id);
-    elementStore.detail[id] = page;
+    store.list.push(id);
+    store.detail[id] = page;
   }
-  return { elementStore, addPage };
+
+  function deletePage(id: string) {
+    store.list = store.list.filter(pageId => pageId !== id);
+    delete store.detail[id];
+  }
+
+  function findPageByName(name: string) {
+    const pageList = Object.values(store.detail);
+    return pageList.filter(page => {
+      return page.name.toLowerCase() === name
+    });
+  }
+
+  return { store, addPage, deletePage, findPageByName };
 });
--- src/types/Element.ts
+++ src/types/Element.ts
@@ -14,13 +14,13 @@ interface ElementCommonData {
   x: number; // 엘리먼트가 x축으로 패닝된 수치 (px)
   y: number; // 엘리먼트가 y축으로 패닝된 수치 (px)
   parentId: string | null; // 부모 엘리먼트의 아이디
-  children: Record<string, ChildrenType> | null; // 자식 엘리먼트
+  children: ElementStore<ChildrenType> | null; // 자식 엘리먼트
 };
 
 // 프로젝트 내에 속한 모든 페이지 정보를 저장하는 pinia 스토어 타입 
-interface ElementStore {
+interface ElementStore<T extends ElementCommonData> {
   list: string[];  // 프로젝트 내에 속한 모든 페이지의 아이디 목록
-  detail: Record<string, PageElement>;
+  detail: Record<string, T>;
 }
 
 // 다른 엘리먼트의 자식 엘리먼트가 될 수 있는 엘리먼트 타입
@@ -36,7 +36,7 @@ interface PageElement extends ElementCommonData  {
 
 interface ShapeElement extends ElementCommonData {
   parentId: string;
-  children: Record<string, ChildrenType>;
+  children: ElementStore<ChildrenType>;
   properties: DesignProps;
 };
 
